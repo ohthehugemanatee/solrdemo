18fd49154569790fd5cab5651ea0efe812ed2227
Dávid Andor <david-andor@kozpontiagy.hu>
2015-02-16 22:11:27 +0100

    Issue #5985 - Add example settings.php

---
+++ sites/default/example.settings.local.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * Documentation missing.
+ */
+
+$databases['default']['default']['username'] = 'my_username';
+$databases['default']['default']['password'] = 'my_password';
+$databases['default']['default']['database'] = 'solrbm';
+
+$conf['solrbm_search_api_server__default'] = array(
+  'port' => '8983',
+  'path' => 'solr/solrbm',
+);
