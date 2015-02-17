5a7fae069704462489480019e6692cb377d229f7
Dávid Andor <david-andor@kozpontiagy.hu>
2015-02-17 10:56:11 +0100

    Issue #5985 - Documentation.

--- sites/default/example.settings.local.php
+++ sites/default/example.settings.local.php
@@ -9,6 +9,9 @@ $databases['default']['default']['username'] = 'my_username';
 $databases['default']['default']['password'] = 'my_password';
 $databases['default']['default']['database'] = 'solrbm';
 
+/**
+ * @see solrbm_servers_default_search_api_server()
+ */
 $conf['solrbm_search_api_server__default'] = array(
   'port' => '8983',
   'path' => 'solr/solrbm',
