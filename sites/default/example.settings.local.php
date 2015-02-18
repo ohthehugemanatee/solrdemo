09e410a8f6e7534cf1797111647209d0b9c9ee20
Dávid Andor <david-andor@kozpontiagy.hu>
2015-02-18 09:47:40 +0100

    Issue #5985 - Add module: memcache.

--- sites/default/example.settings.local.php
+++ sites/default/example.settings.local.php
@@ -16,3 +16,14 @@ $conf['solrbm_search_api_server__default'] = array(
   'port' => '8983',
   'path' => 'solr/solrbm',
 );
+
+$conf['cache_backends'][] = 'profiles/solrbmp/modules/contrib/memcache/memcache.inc';
+$conf['lock_inc'] = 'profiles/solrbmp/modules/contrib/memcache/memcache-lock.inc';
+
+// The 'cache_form' bin must be assigned to non-volatile storage.
+$conf['cache_default_class'] = 'MemCacheDrupal';
+$conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
+
+$conf['memcache_servers'] = array(
+  'localhost:11211' => 'default',
+);
